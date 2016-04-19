950ce0538b15648aed2c84b5d66e27b02189f513
Tyler Lugger <tylu3495@colorado.edu>
2016-04-18 18:51:15 -0600

    added test case for finding area of triangle given coordinates

--- geometry_test.c
+++ geometry_test.c
@@ -145,7 +145,11 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
-START_TEST(test_coord_2d_area_triangle) {
+START_TEST(test_2d_area) {
+
+   coord_2d_t a;
+   coord_2d_t b;
+   coord_2d_t c;
 
    a.x = 5;
    a.y = 5;
@@ -153,6 +157,9 @@ START_TEST(test_coord_2d_area_triangle) {
    b.y = 15;
    c.x = 10;
    c.y = 5;
+  ck_assert(coord_2d_area_triangle(&a,&b,&c) == 25);
+}
+END_TEST
 
 
 /* coord_2d Test Suite */
@@ -172,10 +179,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area = tcase_create("coord_2d_area");
+    tcase_add_test(tc_2d_area, test_2d_area);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area);
 
     /* Return Suite */
     return s;
